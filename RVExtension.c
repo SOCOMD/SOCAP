5738a3313851ed3a078928db2014038893fde1e5
Nathan Chambers <nathan.m.chambers@gmail.com>
2020-02-06 18:42:54 +1100

    Adjust dll entry point for Win/GNU

--- RVExtension.c
+++ RVExtension.c
@@ -4,29 +4,22 @@ extern void goRVExtension(char *output, size_t outputSize, char *input);
 extern void goRVExtensionVersion(char *output, size_t outputSize);
 extern void goRVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc);
 
-#ifdef WIN64
-void RVExtension(char *output, size_t outputSize, char *input) {
-	goRVExtension(output, outputSize, input);
-}
- 
-void RVExtensionVersion(char *output, size_t outputSize) {
-	goRVExtensionVersion(output, outputSize);
-}
- 
-void RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
-	goRVExtensionArgs(output, outputSize, input, argv, argc);
-}
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define DLL_PUBLIC __declspec(dllexport)
 #else
-__declspec(dllexport) void __stdcall _RVExtension(char *output, size_t outputSize, char *input) {
+    //  GCC
+    #define DLL_PUBLIC __attribute__((visibility("default")))
+#endif
+
+DLL_PUBLIC void _RVExtension(char *output, size_t outputSize, char *input) {
   goRVExtension(output, outputSize, input);
 }
 
-__declspec(dllexport) void __stdcall _RVExtensionVersion(char *output, size_t outputSize) {
+DLL_PUBLIC void _RVExtensionVersion(char *output, size_t outputSize) {
   goRVExtensionVersion(output, outputSize);
 }
 
-__declspec(dllexport) void __stdcall _RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
+DLL_PUBLIC void _RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
   goRVExtensionArgs(output, outputSize, input, argv, argc);
 }
-#endif
-// do this for all the other exported functions
