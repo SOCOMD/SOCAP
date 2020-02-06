562274ddb176bf8227975c301592516f200d457c
Nathan Chambers <nathan.m.chambers@gmail.com>
2020-02-06 22:58:28 +1100

    Trying again.

--- RVExtension.c
+++ RVExtension.c
@@ -4,29 +4,10 @@ extern void goRVExtension(char *output, size_t outputSize, char *input);
 extern void goRVExtensionVersion(char *output, size_t outputSize);
 extern void goRVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc);
 
-// #if defined(_WIN32) || defined(_WIN64)
-// // __declspec(dllexport) void __stdcall _RVExtension(char *output, size_t outputSize, char *input) {
-// //   goRVExtension(output, outputSize, input);
-// // }
-
-// #else
-// // __attribute__((visibility("default"))) void _RVExtension(char *output, size_t outputSize, char *input) {
-// //   goRVExtension(output, outputSize, input);
-// // }
-// __attribute__ ((visibility ("default"))) void RVExtension(char *output, size_t outputSize, char *input) {
-// 	goRVExtension(output, outputSize, input);
+#if defined(_WIN32) || defined(_WIN64)
+// __declspec(dllexport) void __stdcall _RVExtension(char *output, size_t outputSize, char *input) {
+//   goRVExtension(output, outputSize, input);
 // }
- 
-// __attribute__ ((visibility ("default"))) void RVExtensionVersion(char *output, size_t outputSize) {
-// 	goRVExtensionVersion(output, outputSize);
-// }
- 
-// __attribute__ ((visibility ("default"))) void RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
-// 	goRVExtensionArgs(output, outputSize, input, argv, argc);
-// }
-// #endif
-
-
 void RVExtension(char *output, size_t outputSize, char *input) {
 	goRVExtension(output, outputSize, input);
 }
@@ -38,3 +19,19 @@ void RVExtensionVersion(char *output, size_t outputSize) {
 void RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
 	goRVExtensionArgs(output, outputSize, input, argv, argc);
 }
+#else
+// __attribute__((visibility("default"))) void _RVExtension(char *output, size_t outputSize, char *input) {
+//   goRVExtension(output, outputSize, input);
+// }
+__attribute__ ((visibility ("default"))) void RVExtension(char *output, size_t outputSize, char *input) {
+	goRVExtension(output, outputSize, input);
+}
+ 
+__attribute__ ((visibility ("default"))) void RVExtensionVersion(char *output, size_t outputSize) {
+	goRVExtensionVersion(output, outputSize);
+}
+ 
+__attribute__ ((visibility ("default"))) void RVExtensionArgs(char* output, size_t outputSize, char* input, char** argv, int argc) {
+	goRVExtensionArgs(output, outputSize, input, argv, argc);
+}
+#endif
